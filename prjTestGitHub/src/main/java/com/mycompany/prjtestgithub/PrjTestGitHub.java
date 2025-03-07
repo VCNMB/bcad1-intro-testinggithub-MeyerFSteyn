e2dc4947ea973aa6dbce2129396561b94e4358ee
MeyerFSteyn <ST10471137@vcconnect.edu.za>
2025-03-07 11:54:31 +0200

    Started working on classes

--- prjTestGitHub/src/main/java/com/mycompany/prjtestgithub/PrjTestGitHub.java
+++ prjTestGitHub/src/main/java/com/mycompany/prjtestgithub/PrjTestGitHub.java
@@ -8,9 +8,32 @@ package com.mycompany.prjtestgithub;
  *
  * @author mryer
  */
+// identify 3 diffrent classes that might contain each of these objects
+// A goldent retrever dog names name Maple
+// The eiffel tower
+// A novel titled "Pride and prejudice"
+
+// Identify 3 objects that might belong to each of the following classes
+// Computer
+// Fictional Character
+// Sport
+
+// PET, DOG, VAT Info
+// LANDMARK, TourestAtraction, BIGTOWER 
+// NOVEL, BOOKS, Jane Austion Books
+
+// Components, Windoes, HP
+// Name, Age, Where thay are from
+// Rugby, Tennis, Golf 
+
+
 public class PrjTestGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hello World! I just did my first github project");
-    }
+        
+        newClass nc = new newClass();
+        nc.helloClass();
+        
+    }   
 }
---
+++ prjTestGitHub/src/main/java/com/mycompany/prjtestgithub/newClass.java
@@ -0,0 +1,19 @@
+package com.mycompany.prjtestgithub;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mryer
+ */
+public class newClass {
+
+    public void helloClass() {
+        System.out.println("Hello from the 2nd class!");
+        
+    }
+    
+}
